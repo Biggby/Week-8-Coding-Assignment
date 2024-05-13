e56c0c2dcaefd6a037e2e45fb68c837bed71bcde
Water <81544555+Biggby@users.noreply.github.com>
2024-05-13 13:02:32 -0400

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,7 +1,7 @@
 class MenuItem {
-    constructor(name, category) {
+    constructor(name, category) { //parameters
       this.name = name;
-      this.category = category;
+      this.category = category; // Food & Drink
     }
 }
   
@@ -10,9 +10,9 @@ class MenuItem {
       this.items = [];
     }
     addItem(item) {
-      this.items.push(item);
+      this.items.push(item); //.push is what pushes the items to the menu
     }
-    displayMenu() {
+    displayMenu() { // method returns menu into console
       let menuInfo = "";
       for (let item of this.items) {
         menuInfo += `${item.name} - ${item.category}\n`;
@@ -21,16 +21,18 @@ class MenuItem {
     }
   }
   
-  let item1 = new MenuItem("Quesadilla Burger", "Food");
-  let item2 = new MenuItem("Cheese Fries", "Food");
-  let item3 = new MenuItem("Dr. Pepper", "Drink");
-  let item4 = new MenuItem("Sparkling Water", "Drink")
+  let item1 = new MenuItem("Quesadilla Burger", "Food" + " $15"); // defines the new  Items
+  let item2 = new MenuItem("Cheese Fries", "Food" + " $9");
+  let item3 = new MenuItem("Dr. Pepper", "Drink" + " $4");
+  let item4 = new MenuItem("Sparkling Water", "Drink" + " $3.50");
+  let item5 = new MenuItem("Chicken Tortillla", "Soup" + " $10")
   
-  let menu = new Menu();
-  menu.addItem(item1);
+  let menu = new Menu(); // commands below push item's to menu
+  menu.addItem(item1);  
   menu.addItem(item2);
   menu.addItem(item3);
   menu.addItem(item4);
+  menu.addItem(item5);
   
   console.log("Menu:");
   console.log(menu.displayMenu());
